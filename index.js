563a554097a441d3305f270f536ef34cfaf073f4
dylanstoudt <138310076+dylanstoudt@users.noreply.github.com>
2023-08-26 19:01:39 -0400

    Added inquirer prompt with a list to toggle between actions, created if/else if statements for each action and added code to terminate program, program doesnt seem to terminate properly

--- index.js
+++ index.js
@@ -3,14 +3,57 @@ const inquirer = require('inquirer')
 const mySQL = require('mysql2')
 
 // Connect to created db, if using a different user/password please update
-const db = mysql.createConnection(
+const db = mySQL.createConnection(
     {
       host: 'localhost',
       user: 'root',
       password: '',
       database: 'employee_tracker_db'
     },
-    console.log(`Connected to the classlist_db database.`)
+    console.log(`Connected to the employee_tracker_db database.`),
   );
 
-let 
+  let employee_tracker = function () {
+    inquirer.prompt([{
+        type: 'list',
+        name: 'toggle',
+        message: 'Please select an action:',
+        choices: ['View Departments', 'View Roles', 'View Employees', 'Add a department', 'Add a role', 'Add an employee', 'Update employees role', 'LOG OUT']
+    }])
+    .then((answers) => {
+        if (answers.prompt === 'View Departments') {
+
+            employee_tracker();
+        }
+        else if (answers.prompt === 'View Roles'){
+            
+            employee_tracker();
+        }
+        else if (answers.prompt === 'View Employees'){
+
+            employee_tracker();
+        }
+        else if (answers.prompt === 'Add a department'){
+
+            employee_tracker();
+        }
+        else if (answers.prompt === 'Add a role'){
+
+            employee_tracker();
+        }
+        else if (answers.prompt === 'Add an employee'){
+
+            employee_tracker();
+        }
+        else if (answers.prompt === 'Update employees role'){
+
+            employee_tracker();
+        }
+        else if (answers.prompt === 'LOG OUT'){
+            db.end()
+            console.log('Program terminated')
+        }
+    })
+  }
+
+  employee_tracker();
