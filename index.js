ba4c084ba1cc178c98d0206787bdd160571be568
dylanstoudt <138310076+dylanstoudt@users.noreply.github.com>
2023-08-27 23:09:24 -0400

    Created code to view dept, roles, and employees in the form of a table in the CLI

--- index.js
+++ index.js
@@ -3,57 +3,71 @@ const inquirer = require('inquirer')
 const mySQL = require('mysql2')
 
 // Connect to created db, if using a different user/password please update
-const db = mySQL.createConnection(
-    {
-      host: 'localhost',
-      user: 'root',
-      password: '',
-      database: 'employee_tracker_db'
-    },
-    console.log(`Connected to the employee_tracker_db database.`),
-  );
+const db = mySQL.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'employee_tracker_db'
+});
 
-  let employee_tracker = function () {
+db.connect((err) => {
+    if (err) throw (err);
+    console.log(`Connected to the employee_tracker_db database.`);
+    employee_tracker();
+}
+)
+
+let employee_tracker = function () {
     inquirer.prompt([{
         type: 'list',
         name: 'toggle',
         message: 'Please select an action:',
         choices: ['View Departments', 'View Roles', 'View Employees', 'Add a department', 'Add a role', 'Add an employee', 'Update employees role', 'LOG OUT']
     }])
-    .then((answers) => {
-        if (answers.prompt === 'View Departments') {
+        .then((answer) => {
+            if (answer.toggle === 'View Departments') {
+                db.query('SELECT * FROM department', (err, res) => {
+                    if (err) throw (err);
+                    console.log('Viewing departments: ')
+                    console.table(res);
+                    employee_tracker();
+                })
+            }
+            if (answer.toggle === 'View Roles') {
+                db.query('SELECT * FROM role', (err, res) => {
+                    if (err) throw (err);
+                    console.log('Viewing roles:')
+                    console.table(res);
+                    employee_tracker();
+                })
+            }
+            if (answer.toggle === 'View Employees') {
+                db.query('SELECT * FROM employee', (err, res) => {
+                    if (err) throw (err);
+                    console.log('Viewing employees:')
+                    console.table(res);
+                    employee_tracker();
+                })
+            }
+            else if (answer.toggle === 'Add a department') {
 
-            employee_tracker();
-        }
-        else if (answers.prompt === 'View Roles'){
-            
-            employee_tracker();
-        }
-        else if (answers.prompt === 'View Employees'){
+                employee_tracker();
+            }
+            else if (answer.toggle === 'Add a role') {
 
-            employee_tracker();
-        }
-        else if (answers.prompt === 'Add a department'){
+                employee_tracker();
+            }
+            else if (answer.toggle === 'Add an employee') {
 
-            employee_tracker();
-        }
-        else if (answers.prompt === 'Add a role'){
+                employee_tracker();
+            }
+            else if (answer.toggle === 'Update employees role') {
 
-            employee_tracker();
-        }
-        else if (answers.prompt === 'Add an employee'){
-
-            employee_tracker();
-        }
-        else if (answers.prompt === 'Update employees role'){
-
-            employee_tracker();
-        }
-        else if (answers.prompt === 'LOG OUT'){
-            db.end()
-            console.log('Program terminated')
-        }
-    })
-  }
-
-  employee_tracker();
+                employee_tracker();
+            }
+            else if (answer.toggle === 'LOG OUT') {
+                db.end();
+                console.log('Program terminated');
+            }
+        })
+}
